75981cfc7e9d76dfbde1507fffae0164bd8f1532
touyou <fujiyou1121@gmail.com>
2020-01-13 16:44:32 +0900

    remove duplicate project

--- rust-sandbox/rust_tutorial/hello_cargo/target/debug/deps/hello_cargo-dda01d1e560fb586.d
+++
@@ -1,5 +0,0 @@
-/Users/touyou/Projects/Playground/rust-sandbox/rust_tutorial/hello_cargo/target/debug/deps/hello_cargo-dda01d1e560fb586: src/main.rs
-
-/Users/touyou/Projects/Playground/rust-sandbox/rust_tutorial/hello_cargo/target/debug/deps/hello_cargo-dda01d1e560fb586.d: src/main.rs
-
-src/main.rs:
--- rust-sandbox/rust_tutorial/hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/Users/touyou/Projects/Playground/rust-sandbox/rust_tutorial/hello_cargo/target/debug/hello_cargo: /Users/touyou/Projects/Playground/rust-sandbox/rust_tutorial/hello_cargo/src/main.rs
