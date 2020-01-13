e032e3fec309ea6678bc71da9ac7841ae697d939
touyou <fujiyou1121@gmail.com>
2020-01-13 16:40:50 +0900

    rust tutorial vol1

---
+++ rust-sandbox/rust_tutorial/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/touyou/Projects/Playground/rust-sandbox/rust_tutorial/hello_cargo/target/debug/hello_cargo: /Users/touyou/Projects/Playground/rust-sandbox/rust_tutorial/hello_cargo/src/main.rs
